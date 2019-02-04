d187a33092ba9760b9d8344f28822740cd97e938
Nicolas KELLUM <nkellum@e2r8p16.42.fr>
2019-02-04 13:01:36 +0100

    starting to put things to the norm

--- line.c
+++ line.c
@@ -6,69 +6,64 @@
 /*   By: nkellum <nkellum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 16:09:46 by nkellum           #+#    #+#             */
-/*   Updated: 2019/01/31 14:46:43 by nkellum          ###   ########.fr       */
+/*   Updated: 2019/02/04 13:00:48 by nkellum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int constrain(int val, int min, int max)
+void	plot(int x, int y, t_mlx *mlx, int amplitude)
 {
-	if(val >= max)
-		return (max);
-	if(val <= min)
-		return(min);
-	return (val);
+	int index;
+
+	index = 4 * (y * 1200) + 4 * x;
+	mlx->img_str[index] = ft_constrain(mlx->crazy_rainbow_r
+		+ amplitude, 0, 255);
+	mlx->img_str[index + 1] = ft_constrain(mlx->crazy_rainbow_g
+		+ amplitude * 2, 0, 255);
+	mlx->img_str[index + 2] = ft_constrain(mlx->crazy_rainbow_b
+		+ amplitude / 2, 0, 255);
 }
 
-void fill_pixel(t_mlx *mlx, int x, int y, int amplitude)
+void	lineloop(t_mlx *mlx, int amplitude)
 {
-    int index = 4 * (y * 1200) + 4 * x;
-    mlx->img_str[index] =  constrain(mlx->crazy_rainbow_r + amplitude, 0, 255);
-    mlx->img_str[index + 1] =  constrain(mlx->crazy_rainbow_g + amplitude * 2, 0, 255);
-    mlx->img_str[index + 2] =  constrain(mlx->crazy_rainbow_b + amplitude / 2, 0, 255);
+	int x;
+
+	x = mlx->xpxl1;
+	while (x <= mlx->xpxl2)
+	{
+		if (mlx->steep)
+		{
+			plot(ipart(mlx->intersectY), x,  mlx, amplitude);
+			plot(ipart(mlx->intersectY) - 1, x, mlx, amplitude);
+		}
+		else
+		{
+			plot(x, ipart(mlx->intersectY), mlx, amplitude);
+			plot(x, ipart(mlx->intersectY) - 1, mlx, amplitude);
+		}
+		mlx->intersectY += mlx->gradient;
+		x++;
+	}
 }
 
-void plot(int x, int y, t_mlx *mlx, int amplitude)
+int		line_in_window(int x0, int y0, int x1, int y1)
 {
-  fill_pixel(mlx, x, y, amplitude);
+	if(x0 >= 1200 || x0 <= 0 || y0 >= 700 || y0 <= 0
+	|| x1 >= 1200 || x1 <= 0 || y1 >= 700 || y1 <= 0)
+		return (0);
+	return (1);
 }
 
-double ipart(double x)
+void	drawLine(int x0, int y0, int x1, int y1, int amplitude, t_mlx *mlx)
 {
-  return floor(x);
-}
+		float dx;
+		float dy;
 
-double round(double x)
-{
-  return ipart(x + 0.5);
-}
-
-double fpart(double x)
-{
-  return  x - floor(x);
-}
-
-double rfpart(double x)
-{
-  return 1 - fpart(x);
-}
-
-void swap(int *x, int *y)
-{
-  int temp = *x;
-  *x = *y;
-  *y = temp;
-}
-
-void drawLine(int x0 , int y0 , int x1 , int y1, int amplitude, t_mlx *mlx)
-{
-		if(x0 >= 1200 || x0 <= 0 || y0 >= 700 || y0 <= 0
-		|| x1 >= 1200 || x1 <= 0 || y1 >= 700 || y1 <= 0)
+		mlx->steep = abs(y1 - y0) > abs(x1 - x0);
+		if(!line_in_window(x0, y0, x1, y1))
 			return;
-    int steep = abs(y1 - y0) > abs(x1 - x0) ;
-
-    if (steep)
+    if (mlx->steep)
     {
         swap(&x0 , &y0);
         swap(&x1 , &y1);
@@ -78,36 +73,13 @@ void drawLine(int x0 , int y0 , int x1 , int y1, int amplitude, t_mlx *mlx)
         swap(&x0 ,&x1);
         swap(&y0 ,&y1);
     }
-
-    float dx = x1-x0;
-    float dy = y1-y0;
-    float gradient = dy/dx;
+    dx = x1-x0;
+    dy = y1-y0;
+    mlx->gradient = dy/dx;
     if (dx == 0.0)
-        gradient = 1;
-
-    int xpxl1 = x0;
-    int xpxl2 = x1;
-    float intersectY = y0;
-
-    if (steep)
-    {
-        int x;
-        for (x = xpxl1 ; x <=xpxl2 ; x++)
-        {
-            plot(ipart(intersectY), x,  mlx, amplitude);
-            plot(ipart(intersectY)-1, x, mlx, amplitude);
-            intersectY += gradient;
-        }
-    }
-    else
-    {
-        int x;
-        for (x = xpxl1 ; x <=xpxl2 ; x++)
-        {
-            plot(x, ipart(intersectY), mlx, amplitude);
-            plot(x, ipart(intersectY)-1, mlx, amplitude);
-            intersectY += gradient;
-        }
-    }
-
+        mlx->gradient = 1;
+     mlx->xpxl1 = x0;
+     mlx->xpxl2 = x1;
+     mlx->intersectY = y0;
+		 lineloop(mlx, amplitude);
 }
